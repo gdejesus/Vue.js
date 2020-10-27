ddd719ba3932ec2e9d1b726af0d2a0beab9d862a
gdejesus <guille17@gmail.com>
2020-10-27 20:33:38 -0300

    funciones pasando argumento.

---
+++ basics-03-events-starting-code/appRefactored.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            counter: 0,
+        };
+    },
+    methods: {
+        add(number) {
+            this.counter += number;
+        },
+        reduce(number) {
+            this.counter -= number;
+        }
+    }
+});
+
+app.mount('#events');
